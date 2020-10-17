7deac4f5fa885b1aa5751c5678a174dcfbebd2c2
Phayao Boonon <phayaob@gmail.com>
2020-10-17 23:03:51 +0700

    init project

---
+++ src/main/java/io/digitalready/billary/BillaryServiceApplication.java
@@ -0,0 +1,13 @@
+package io.digitalready.billary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BillaryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BillaryServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/digitalready/billary/BillaryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.digitalready.billary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BillaryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
